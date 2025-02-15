97bf1993634412938e28838fffea9ae0079bb1ae
mahmoudgshake <mahmoud_gamal1430@yahoo.com>
2025-02-15 18:25:34 +0200

    Added Sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))  # Ensure your package can be imported
+
+project = "Egyptian"
+author = "Your Name"
+release = "0.1"
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+
+html_theme = "alabaster"
